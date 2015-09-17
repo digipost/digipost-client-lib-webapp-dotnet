ff67294483769b8e3b7eb96ae1de9b4ccc1f8e3d
Kristian Enge <kristian.ienge@gmail.com>
2015-09-17 13:24:59 +0200

    Fjern kode og lag tomt view for search

---
+++ DigipostClientLibWebapp/Controllers/SearchController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DigipostClientLibWebapp.Controllers
+{
+    public class SearchController : Controller
+    {
+        public ActionResult Search()
+        {
+            return View();
+        }
+    }
+}
--- DigipostClientLibWebapp/Startup.cs
+++ DigipostClientLibWebapp/Startup.cs
@@ -8,7 +8,6 @@ namespace DigipostClientLibWebapp
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
