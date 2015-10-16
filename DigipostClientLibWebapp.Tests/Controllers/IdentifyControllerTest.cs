d427f3ea3f8dc76d558b97861a3baab5cddb685d
Kristian <kristian.ienge@gmail.com>
2015-10-16 13:21:37 +0200

    Moq digipostService

--- DigipostClientLibWebapp.Tests/Controllers/IdentifyControllerTest.cs
+++ DigipostClientLibWebapp.Tests/Controllers/IdentifyControllerTest.cs
@@ -3,13 +3,17 @@ using Digipost.Api.Client.Domain.Enums;
 using Digipost.Api.Client.Domain.Identify;
 using Digipost.Api.Client.Domain.SendMessage;
 using DigipostClientLibWebapp.Controllers;
+using DigipostClientLibWebapp.Services.Digipost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace DigipostClientLibWebapp.Tests.Controllers
 {
@@ -35,17 +39,33 @@ namespace DigipostClientLibWebapp.Tests.Controllers
         public void Identify()
         {
             // Arrange
-            IdentifyController controller = new IdentifyController();
-            
             Identification identification = new Identification(new RecipientById(IdentificationType.DigipostAddress, digipostAddress));
+            IdentificationResult identificationResult = new IdentificationResult(IdentificationResultType.DigipostAddress, digipostAddress);
+            IdentifyController controller =  IdentifyControllerWithMockedDigipostServiceAndSessionState(identification,identificationResult);
+            
+            
             // Act
-            ViewResult result = controller.Identify(identification) as ViewResult;
-
+            ViewResult result = controller.Identify(identification).Result as ViewResult;
+            
             // Assert
             Assert.IsNotNull(result);
             Assert.IsInstanceOfType(result.Model, typeof(IdentificationResult));
             var resultModel = result.Model as IdentificationResult;
             Assert.AreEqual(digipostAddress, resultModel.Data);
+        }
+
+        private static IdentifyController IdentifyControllerWithMockedDigipostServiceAndSessionState(Identification identification,
+         IdentificationResult identificationResult)
+        {
+            var digipostService = new Mock<DigipostService>();
+            digipostService.Setup(x => x.Identify(identification)).ReturnsAsync(identificationResult);
+            var controller = new IdentifyController(digipostService.Object);
+            var context = new Mock<HttpContextBase>();
+            var session = new Mock<HttpSessionStateBase>();
+            context.Setup(x => x.Session).Returns(session.Object);
+            var requestContext = new RequestContext(context.Object, new RouteData());
+            controller.ControllerContext = new ControllerContext(requestContext, controller);
+            return controller;
         }
 
     }
--- DigipostClientLibWebapp/Controllers/IdentifyController.cs
+++ DigipostClientLibWebapp/Controllers/IdentifyController.cs
@@ -1,11 +1,22 @@
 ﻿using System.Web.Mvc;
 using Digipost.Api.Client.Domain.Enums;
 using Digipost.Api.Client.Domain.Identify;
+using DigipostClientLibWebapp.Services.Digipost;
+using System.Threading.Tasks;
 
 namespace DigipostClientLibWebapp.Controllers
 {
     public class IdentifyController : ControllerBase
     {
+        public IdentifyController() : base()
+        {
+
+        }
+        public IdentifyController(DigipostService digipostService) : base(digipostService)
+        {
+
+        }
+
         // GET: Identify
         public ActionResult Index()
         {
@@ -14,9 +25,9 @@ namespace DigipostClientLibWebapp.Controllers
         }
 
         // GET: Identify/Identify/
-        public ActionResult Identify(Identification identification)
+        public async Task<ActionResult> Identify(Identification identification)
         {
-            var result = new IdentificationResult(IdentificationResultType.DigipostAddress, "test");
+            var result = await GetDigipostService().Identify(identification);
 
             return View("IdentificationResult",result);
         } 
