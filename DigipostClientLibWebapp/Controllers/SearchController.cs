ff590fdf103f0dc40f9d5d8124e80a13ac926b30
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-10-23 12:48:30 +0200

    Legger til en egen sessionhåndterer som er generisk

--- DigipostClientLibWebapp/Controllers/SearchController.cs
+++ DigipostClientLibWebapp/Controllers/SearchController.cs
@@ -4,6 +4,7 @@ using System.Web.Mvc;
 using Digipost.Api.Client.Domain.Search;
 using DigipostClientLibWebapp.Constants;
 using DigipostClientLibWebapp.Services.Digipost;
+using DigipostClientLibWebapp.Utilities;
 
 namespace DigipostClientLibWebapp.Controllers
 {
@@ -21,25 +22,22 @@ namespace DigipostClientLibWebapp.Controllers
 
         public ActionResult Index(List<SearchDetails> search)
         {
-            if (search == null)
-                return View();
-
             return View(search);
         }
-        
+
         [HttpPost]
         public async Task<ActionResult> Search(string search)
         {
             var searchResult = await GetDigipostService().Search(search);
-            
-            Session[SessionConstants.PersonDetails] = searchResult;
+
+            SessionManager.AddToSession(searchResult);
 
             return View("Index",searchResult.PersonDetails); 
         }
 
         public ActionResult GoToSend(string digipostAddress)
         {
-            var personDetails = (ISearchDetailsResult)Session[SessionConstants.PersonDetails];
+            var personDetails = SessionManager.GetFromSession<ISearchDetailsResult>();
             var person = personDetails.PersonDetails.Find(details => details.DigipostAddress.Equals(digipostAddress));
             
             Session[SessionConstants.PersonModel] = person;
---
+++ DigipostClientLibWebapp/Utilities/SessionManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+using Digipost.Api.Client.Domain.Search;
+using DigipostClientLibWebapp.Constants;
+
+namespace DigipostClientLibWebapp.Utilities
+{
+    public static class SessionManager
+    {
+        public static T GetFromSession<T>()
+        {
+            return (T) System.Web.HttpContext.Current.Session[SessionConstants.PersonDetails];
+        }
+
+        public static void AddToSession<T>(T value)
+        {
+            System.Web.HttpContext.Current.Session[SessionConstants.PersonDetails] = value;
+        }
+    }
+}
