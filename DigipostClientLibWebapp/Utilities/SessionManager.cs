b3d54915b751b5764a871850d072ce69ca42c9d6
Aleksander Aas Sjåfjell <aleksander.sjafjell@bekk.no>
2015-10-23 13:16:34 +0200

    Ta inn nøkkel i sessionmanger

--- DigipostClientLibWebapp/Utilities/SessionManager.cs
+++ DigipostClientLibWebapp/Utilities/SessionManager.cs
@@ -6,16 +6,21 @@ namespace DigipostClientLibWebapp.Utilities
 {
     public static class SessionManager
     {
-        public static T GetFromSession<T>()
+        public static T GetFromSession<T>(string key)
         {
-            var sessionData = System.Web.HttpContext.Current.Session[SessionConstants.PersonDetails];
+            var sessionData = System.Web.HttpContext.Current.Session[key];
             
             return sessionData == null ? default(T) : (T) sessionData;
         }
 
-        public static void AddToSession<T>(T value)
+        public static void AddToSession<T>(string key, T value)
         {
-            System.Web.HttpContext.Current.Session[SessionConstants.PersonDetails] = value;
+            System.Web.HttpContext.Current.Session[key] = value;
+        }
+
+        public static void RemoveFromSession(string key)
+        {
+            System.Web.HttpContext.Current.Session.Remove(key);
         }
     }
 }
